8fd12d28d0a26c323e669efb8f96a65d2aad5636
Jefrinsson J F Calderón <84988608+FC4rCode@users.noreply.github.com>
2021-07-13 09:24:24 -0500

    Agregar archivos de proyecto.

---
+++ Huella/Form1.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtTemplate = New System.Windows.Forms.TextBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnRegister = New System.Windows.Forms.Button()
+        Me.fpicture = New System.Windows.Forms.PictureBox()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.deviceSerial = New System.Windows.Forms.Label()
+        Me.prompt = New System.Windows.Forms.Label()
+        Me.btnVerify = New System.Windows.Forms.Button()
+        CType(Me.fpicture, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtTemplate
+        '
+        Me.txtTemplate.Location = New System.Drawing.Point(502, 263)
+        Me.txtTemplate.Multiline = True
+        Me.txtTemplate.Name = "txtTemplate"
+        Me.txtTemplate.Size = New System.Drawing.Size(424, 82)
+        Me.txtTemplate.TabIndex = 31
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(345, 367)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(99, 21)
+        Me.btnClear.TabIndex = 29
+        Me.btnClear.Text = "&Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnRegister
+        '
+        Me.btnRegister.Location = New System.Drawing.Point(502, 116)
+        Me.btnRegister.Name = "btnRegister"
+        Me.btnRegister.Size = New System.Drawing.Size(95, 39)
+        Me.btnRegister.TabIndex = 30
+        Me.btnRegister.Text = "&Register"
+        Me.btnRegister.UseVisualStyleBackColor = True
+        '
+        'fpicture
+        '
+        Me.fpicture.BackColor = System.Drawing.SystemColors.Window
+        Me.fpicture.Location = New System.Drawing.Point(176, 105)
+        Me.fpicture.Name = "fpicture"
+        Me.fpicture.Size = New System.Drawing.Size(268, 283)
+        Me.fpicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.fpicture.TabIndex = 28
+        Me.fpicture.TabStop = False
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Location = New System.Drawing.Point(499, 247)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(57, 13)
+        Me.label1.TabIndex = 25
+        Me.label1.Text = "Template: "
+        '
+        'deviceSerial
+        '
+        Me.deviceSerial.AutoSize = True
+        Me.deviceSerial.Location = New System.Drawing.Point(499, 225)
+        Me.deviceSerial.Name = "deviceSerial"
+        Me.deviceSerial.Size = New System.Drawing.Size(76, 13)
+        Me.deviceSerial.TabIndex = 26
+        Me.deviceSerial.Text = "Device Serial: "
+        '
+        'prompt
+        '
+        Me.prompt.AutoSize = True
+        Me.prompt.Location = New System.Drawing.Point(173, 401)
+        Me.prompt.Name = "prompt"
+        Me.prompt.Size = New System.Drawing.Size(50, 13)
+        Me.prompt.TabIndex = 27
+        Me.prompt.Text = "Ready ..."
+        '
+        'btnVerify
+        '
+        Me.btnVerify.Enabled = False
+        Me.btnVerify.Location = New System.Drawing.Point(603, 117)
+        Me.btnVerify.Name = "btnVerify"
+        Me.btnVerify.Size = New System.Drawing.Size(95, 38)
+        Me.btnVerify.TabIndex = 24
+        Me.btnVerify.Text = "&Verify"
+        Me.btnVerify.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1098, 519)
+        Me.Controls.Add(Me.txtTemplate)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnRegister)
+        Me.Controls.Add(Me.fpicture)
+        Me.Controls.Add(Me.label1)
+        Me.Controls.Add(Me.deviceSerial)
+        Me.Controls.Add(Me.prompt)
+        Me.Controls.Add(Me.btnVerify)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.fpicture, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private WithEvents txtTemplate As TextBox
+    Private WithEvents btnClear As Button
+    Private WithEvents btnRegister As Button
+    Private WithEvents fpicture As PictureBox
+    Private WithEvents label1 As Label
+    Private WithEvents deviceSerial As Label
+    Private WithEvents prompt As Label
+    Private WithEvents btnVerify As Button
+End Class
---
+++ Huella/Form1.vb
@@ -0,0 +1,100 @@
+﻿Imports AxZKFPEngXControl
+
+Public Class Form1
+
+    Private ZkFprint As New AxZKFPEngX()
+    Private Check As Boolean
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            Controls.Add(ZkFprint)
+            InitialAxZkfp()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+
+
+    End Sub
+
+    Private Sub InitialAxZkfp()
+        Try
+
+            AddHandler ZkFprint.OnImageReceived, AddressOf zkFprint_OnImageReceived
+            AddHandler ZkFprint.OnFeatureInfo, AddressOf zkFprint_OnFeatureInfo
+            AddHandler ZkFprint.OnEnroll, AddressOf zkFprint_OnEnroll
+            AddHandler ZkFprint.OnCapture, AddressOf zkFprint_OnCapture
+
+            If (ZkFprint.InitEngine = 0) Then
+                ZkFprint.FPEngineVersion = "9"
+                ZkFprint.EnrollCount = 3
+                deviceSerial.Text = (deviceSerial.Text + (" " _
+                            + (ZkFprint.SensorSN + ("Count: " _
+                            + (ZkFprint.SensorCount.ToString + ("Index: " + ZkFprint.SensorIndex.ToString))))))
+                ShowHintInfo("Dispositivo conectado correctamente.")
+            End If
+        Catch ex As Exception
+            ShowHintInfo(("Error de inicio del dispositivo, error: " + ex.Message))
+        End Try
+    End Sub
+    Private Sub zkFprint_OnImageReceived(sender As Object, e As IZKFPEngXEvents_OnImageReceivedEvent)
+        Dim g As Graphics = fpicture.CreateGraphics()
+        Dim bmp As New Bitmap(fpicture.Width, fpicture.Height)
+        g = Graphics.FromImage(bmp)
+        Dim dc As Integer = g.GetHdc().ToInt32()
+        ZkFprint.PrintImageAt(dc, 0, 0, bmp.Width, bmp.Height)
+        g.Dispose()
+        fpicture.Image = bmp
+    End Sub
+    Private Sub zkFprint_OnFeatureInfo(sender As Object, e As IZKFPEngXEvents_OnFeatureInfoEvent)
+        Dim strTemp As String = String.Empty
+        If ZkFprint.EnrollIndex <> 1 Then
+            If ZkFprint.IsRegister Then
+                If ZkFprint.EnrollIndex - 1 > 0 Then
+                    Dim eindex As Integer = ZkFprint.EnrollIndex - 1
+                    strTemp = "Por favor escanee de nuevo ..." & eindex
+                End If
+            End If
+        End If
+        ShowHintInfo(strTemp)
+    End Sub
+    Private Sub zkFprint_OnEnroll(sender As Object, e As IZKFPEngXEvents_OnEnrollEvent)
+        If e.actionResult Then
+            Dim template As String = ZkFprint.EncodeTemplate1(e.aTemplate)
+            txtTemplate.Text = template
+            ShowHintInfo("Registro exitoso. Puedes verificarlo ahora.")
+            btnRegister.Enabled = False
+            btnVerify.Enabled = True
+        Else
+            ShowHintInfo("Error, por favor regístrate de nuevo.")
+        End If
+    End Sub
+    Private Sub zkFprint_OnCapture(ByVal sender As Object, ByVal e As IZKFPEngXEvents_OnCaptureEvent)
+        Dim template As String = ZkFprint.EncodeTemplate1(e.aTemplate)
+        If ZkFprint.VerFingerFromStr(template, txtTemplate.Text, False, Check) Then
+            ShowHintInfo("Verificado.")
+        Else
+            ShowHintInfo("No Verificado.")
+        End If
+    End Sub
+    Private Sub btnVerify_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnVerify.Click
+        If ZkFprint.IsRegister Then
+            ZkFprint.CancelEnroll()
+        End If
+        'ZkFprint.OnCapture = (ZkFprint.OnCapture + zkFprint_OnCapture())
+        ZkFprint.BeginCapture()
+        ShowHintInfo("Por favor dar muestra de huellas dactilares.")
+    End Sub
+    Private Sub ShowHintInfo(s As String)
+        prompt.Text = s
+    End Sub
+    Private Sub btnRegister_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnRegister.Click
+        ZkFprint.CancelEnroll()
+        ZkFprint.EnrollCount = 3
+        ZkFprint.BeginEnroll()
+        ShowHintInfo("Por favor dar muestra de huellas dactilares.")
+    End Sub
+    Private Sub btnClear_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnClear.Click
+        fpicture.Image = Nothing
+    End Sub
+
+End Class
